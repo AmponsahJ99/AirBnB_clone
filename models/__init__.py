7d2d3a82e24700b4b893d5c6f0affd823d085c82
AmponsahJ99 <amjust99@gmail.com>
2023-10-15 04:57:00 -0700

    AIRBnB Clone

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""
-    Task 5: the module contains a storage variable of type FileStorage
-"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
